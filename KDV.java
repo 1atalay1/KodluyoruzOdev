56093efe92520cb00c4b07d78ec7ef9ad91b9d62
1atalay1 <141409829+1atalay1@users.noreply.github.com>
2023-08-04 13:43:22 +0300

    Add files via upload

---
+++ KDV.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sertifika;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Oğuzhan
+ */
+public class KDV {
+    public static void main(String []args){
+        Scanner input =new Scanner(System.in);
+        System.out.println("KDV'siz fiyatı Girin:");
+        double kdv;// kdv nin yüzdeliği
+        double price=0; //kdvli fiyat
+        double kdvamount=0; //kdv tutarı
+        double wokdv=input.nextDouble(); //kdvsiz fiyat
+        if(wokdv<=1000 && wokdv>=0){
+           kdv=0.18;
+           price=wokdv+(wokdv*kdv);
+           kdvamount=price-wokdv;
+           
+            
+        }
+        else if(wokdv>1000){
+            kdv=0.08;
+            price=wokdv+(wokdv*kdv);
+           kdvamount=price-wokdv;
+        }
+        else {
+            System.out.println("Geçersiz para girişi");
+        }
+        
+        System.out.println("KDV'siz Fiyat :"+wokdv);
+        System.out.println("KDV'li Fiyat: "+price);
+        System.out.println("KDV tutarı: "+kdvamount);
+        
+        
+    }
+}
